1bb537120371fe8ccdf9bb84e45163fcef2a71d1
gedons <gedoniani@gmail.com>
2023-07-17 16:39:09 +0100

    app setup

---
+++ database/migrations/2023_07_17_135235_create_packages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('driver_id')->nullable();
+            $table->foreign('driver_id')->references('id')->on('drivers');
+            $table->string('tracking_number');
+            $table->string('delivery_address');
+            $table->string('delivery_status');
+            $table->string('special_instructions');
+            $table->string('payment_reference')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('packages');
+    }
+};
